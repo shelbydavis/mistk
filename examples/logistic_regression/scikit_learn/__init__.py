90dda9ab6189d52c4aea351966341ae7795d2f88
dcastel1 <dcastel1@binghamton.edu>
2019-05-01 08:26:22 -0400

    updated source code

--- examples/logistic_regression/scikit_learn/__init__.py
+++ examples/logistic_regression/scikit_learn/__init__.py
@@ -15,7 +15,15 @@
 #
 ##############################################################################
 
-"""
-Wrapper python package for obtaining all MISTK Data Model classes. 
-"""
-from mistk.server.models import *  #pylint: disable=wildcard-import
+# -*- coding: utf-8 -*-
+#
+# Content developed by Lockheed Martin ATL for AFRL/RIS 
+# Contract #: FA8750-17-C-0282.
+#
+# Unlimited Government Rights
+#
+# Distribution C.  Distribution authorized to US Government agencies and
+# their contractors Administrative or Operational Use, October 2017.
+# Other requests for this document shall be referred to AFRL/RIS.
+
+
